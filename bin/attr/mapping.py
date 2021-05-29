56d3900c137eac27c3867d1359ec3b72d5d434cc
Allan Bolipata <allan.bolipata@gmail.com>
2021-05-29 00:57:40 -0700

    Updated mapping file

--- bin/attr/mapping.py
+++ bin/attr/mapping.py
@@ -1,6 +1,10 @@
+import sys
+
 class MappingAttr:
    def __init__(self):
         self.sample_id = ""
+        self.run_id = ""
+        self.fcid = ""
 
    def set_attr(self, keys, data):
         """
@@ -11,7 +15,20 @@ class MappingAttr:
         for i,key in enumerate(keys_lower):
             val = data[keys[i]]
             if key == "sample_id":
-                 self.sample_id = val
+                self.sample_id = val
+            if key == "runid_flowcellid":
+                """
+                    run id and fcid are derived from the same
+                    column, but they MUST be delim by underscore
+
+                    we are assuming there are two underscores in
+                    the mapping file
+                """
+                if val.count("_") < 2:
+                    print("Can't get run id/fcid from %s; exiting." % val)
+                    sys.exit(1)
+                self.run_id = "".join(val.split("_")[0:2])
+                self.fcid = val.split("_")[2]
 
    def __repr__(self):
        return "Mapping {sample_id}: ".format(
