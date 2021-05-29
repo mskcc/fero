3c537739c120bc230b7c85af4701dabe1c2ac7f2
Allan Bolipata <allan.bolipata@gmail.com>
2021-05-29 02:41:38 -0700

    Add output of metadata with stdout log of file-metadata json match

--- process_files.py
+++ process_files.py
@@ -1,7 +1,8 @@
 from configparser import SafeConfigParser
 import csv
-from pprint import pprint
 import os
+import json
+from pprint import pprint
 from bin.project import ProjectObj
 
 if __name__ == "__main__":
@@ -15,3 +16,16 @@ if __name__ == "__main__":
     barcode_file = parser.get("DATA", "BarcodeFile")
 
     project_obj = ProjectObj(request_file, pairing_file, mapping_file, data_clinical_file, barcode_file)
+
+    file_metadata = project_obj.generate_metadata_json()
+
+    # make metadata jsons for each file
+    json_path = 'metadata_jsons'
+    if not os.path.exists(json_path):
+        os.makedirs(json_path)
+    for fpath in file_metadata:
+        metadata = file_metadata[fpath]
+        json_out_path = os.path.join(json_path,os.path.basename(fpath) + ".json")
+        json_file = open(json_out_path, 'w')
+        json.dump(metadata, json_file)
+        print(fpath, os.path.abspath(json_out_path))
