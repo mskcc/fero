5e46ebfb89fe18a214cd68f6e527ab2fdb5bd880
Allan Bolipata <bolipatc@mskcc.org>
2021-09-21 15:39:57 -0400

    Spoof barcode script

---
+++ extra/spoof_barcode.py
@@ -0,0 +1,18 @@
+import sys, os
+
+
+def spoof_barcode(sample_run_id):
+    if sample_run_id == 'DMPLibs':
+        return "dmpbc"
+    return "fakebc"
+
+
+if __name__ == '__main__':
+    fname = sys.argv[1]
+    with open(fname, 'r') as f:
+        for line in f:
+            tab = line.split("\t")
+            sample_id = tab[1]
+            sample_run_id = tab[2]
+            bc = spoof_barcode(sample_run_id)
+            print(sample_id, "\t", bc)
