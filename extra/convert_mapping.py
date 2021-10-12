d220944292eeab1afd7a7bee28cd24e99147878d
Allan Bolipata <bolipatc@mskcc.org>
2021-10-12 15:56:08 -0400

    Add pooled normal conversion

--- extra/convert_mapping.py
+++ extra/convert_mapping.py
@@ -8,13 +8,29 @@ import sys, os
 import convert_dmplibs as dmp
 import convert_ifs_archive as ifs
 
+
+def convert_sample_name(s,r,p):
+    # Mimics behavior in .bin.project
+    pn_sample_id = s + "_" + r
+    preservation = "FROZEN"
+    if "ffpe" in s.lower():
+        preservation = "FFPE"
+    if len(pn_sample_id) > 32:
+        pn_sample_id = "PN_{p}_{r}".format(
+                p = preservation,
+                r = r
+                )
+    return pn_sample_id
+
+
 if __name__ == '__main__':
     fname = sys.argv[1]
+    request_id = sys.argv[2]
     with open(fname, 'r') as f:
         for line in f:
             tab = line.split("\t")
             fpath = tab[3]
-            sample_name = tab[1]
+            sample_name = convert_sample_name(tab[1])
             s = "\t".join(tab[0:3])
             path_ifs = False
             if ifs.validate_path(fpath):
